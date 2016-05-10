f17e9a1724eaf181d793fe57344019ed6afb45e3
wdingsjtu <wding.sjtu@gmail.com>
2016-05-10 16:02:48 +0100

    Quick sort.

---
+++ test_random.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+from __future__ import print_function
+import random
+from sorting import quick_sort
+
+
+n = 10
+rand_int_list = [random.randint(0, 20) for _ in range(n)]
+print("A random list:")
+print(rand_int_list, '\n')
+
+print("StandarD results:")
+print(sorted(rand_int_list), '\n')
+
+quick_sort(rand_int_list)
+print("Sorted by my implementation:")
+print(rand_int_list, '\n')
+
+is_identical = True if sorted(rand_int_list) == rand_int_list else False
+print("My result is ", end='')
+print(is_identical)
