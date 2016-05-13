2ef42e96073358abaa74ada0a28a900618df2ab1
wdingsjtu <wding.sjtu@gmail.com>
2016-05-14 00:41:36 +0100

    Complete node attributes.

--- dataStructures.py
+++ dataStructures.py
@@ -8,34 +8,60 @@ Chapter 12: Binary Search Tree (p.161)
 '''
 from __future__ import print_function
 
+
 class BinaryTreeNode(object):
     def __init__(self, value):
         self.value = value
         self._parent = None
-        self.left = None
-        self.right = None
+        self._left = None
+        self._right = None
         self.tree = None
 
     def __repr__(self):
         return str(self.value)
   
-    def _find_parent(self):
-        parent_index = (self.tree.index(self) + 1)//2 - 1
-        if parent_index >= 0:
-            return parent_index
+    def _getter(self, index):
+        if 0 <= index < len(self.tree):
+            return self.tree[index]
 
-    def get_parent(self):
-        if self._find_parent() is not None:
-            return self.tree[self._find_parent()]
-
-    def set_parent(self, node):
+    def _setter(self, index, node):
         if not isinstance(node, type(self)):
             raise TypeError("%s cannot be set as a node." %node)
-        if self._find_parent() is not None:
-            self.tree[self._find_parent()] = node
+        if 0 <= index < len(self.tree):
+            self.tree[index] = node
+
+    def _find_parent(self):
+        ''' Return the index of parent node.
+        '''
+        return (self.tree.index(self) + 1)//2 - 1
+
+    def _find_left(self):
+        return (self.index(node) + 1)*2 - 1
+
+    def _find_right(self):
+        right_index = (self.index(node) + 1)*2
+
+    def get_parent(self):
+        return self._getter(self._find_parent())
+
+    def set_parent(self, node):
+        return self._setter(self._find_parent(), node)
+
+    def get_left(self):
+        return self._getter(self._find_left())
+
+    def get_right(self):
+        return self._getter(self._find_right())
+
+    def set_left(self, node):
+        return self._setter(self._find_left(), node)
+
+    def set_right(self, node):
+        return self._setter(self._find_right(), node)
 
     parent = property(get_parent, set_parent)
-  
+    left = property(get_left, set_left)
+    right = property(get_right, set_right)
 
 class BinaryTree(list):
     ''' A complete binary tree implemented by list (sequence storage).
@@ -50,17 +76,3 @@ class BinaryTree(list):
 
         for nodes in self:
             nodes.tree = self
-       
-#    def left(self, node):
-#        left_index = (self.index(node) + 1)*2 - 1
-#        if left_index > len(self) - 1:
-#            return None
-#        else:
-#            return self[left_index]
-#
-#    def right(self, node):
-#        right_index = (self.index(node) + 1)*2
-#        if right_index > len(self) - 1:
-#            return None
-#        else:
-#            return self[right_index]
